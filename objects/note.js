1e8e96c3884cfef0e3748bbb543eadd636f9276b
Collin Lane <cjlane@ucsc.edu>
2016-01-30 01:25:47 -0800

    created note.js
    - defines the data structure for a “Note”

---
+++ objects/note.js
@@ -0,0 +1,36 @@
+/*
+ * The note class
+ */
+
+var config = require('../config');
+var crypto = require('crypto');
+var mongoose = require('mongoose');
+var async = require('async');
+
+// Structure of the Note class
+var noteSchema = mongoose.Schema({
+	user: { type: String, default: "" },
+	format: { type: Number, required: true },
+	instances: { type: Number, required: true },
+	data: [],
+	name: { type: String, required: true },
+	description: { type: String, default: "" },
+	class_id: { type: Number, required: true },
+	class_name: { type: String, required: true },
+	term_id: { type: Number, required: true },
+	term_name: { type: String, required: true },
+	teacher_id: { type: Number },
+	teacher_name: { type: String },
+	time: { type: Date, required: true },
+	createdAt: { type: Date, default: Date.now },
+	modifiedAt: { type: Date, default: Date.now }
+});
+
+// Aliases object.id to object._id 
+noteSchema.virtual('id').get(function() {
+	return this._id;
+});
+
+// Allows one to use the Note object from another file
+module.exports = mongoose.model('Note', noteSchema);
+
