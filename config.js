5e7609ce95feb3c0bce30943f95b79ba64a8d3a9
Collin Lane <cjlane@ucsc.edu>
2016-01-30 00:29:09 -0800

    created basic config.js & package.json
    - created basic config for node server
    - created basic package.json for node server
    - moved server.js from “server/server.js” into the root folder, it’s
    just easier for file navigation purposes.
    - just realized I should be describing my commits.

---
+++ config.js
@@ -0,0 +1,30 @@
+var fs = require('fs');
+
+// Everything that will be accessible from another script that uses config.js
+module.exports = {
+	auth: {
+		//cert: fs.readFileSync("./ssl/server.crt"),
+		//key: fs.readFileSync("./ssl/server.key"),
+		tokenTTL: 604800, //One week
+		authDelay: 400 // Change based on the computer it's running on... or just make it something relatively high
+	},
+	db: {
+		host: "localhost",
+		name: "test"
+	},
+	log: {
+		logFile: "log.txt"
+	},
+	password: {
+		saltLength: 32,
+		saltWorkFactor: 12,
+	},
+	server: {
+		version: '0.0.1',
+		port: 8080,
+		name: 'SlugNote API'
+	},
+	ticket: {
+		length: 64
+	}
+};
