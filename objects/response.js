78d8bf5452bffa86781e446217431d06734c4799
Collin Lane <cjlane@ucsc.edu>
2016-01-30 01:31:12 -0800

    created basic response.js
    - just for responding to api calls
    - one for success (code 200)
    - one for failure (error code)
    - error codes will be written into routes.js

---
+++ objects/response.js
@@ -0,0 +1,29 @@
+var config = require('../config');
+
+// Usage:
+// var respond = require('/path/to/response');
+// respond(response, status, request, data');
+// ex:
+// 	respond(res, 200, 'login', {
+// 		token: 'blah',
+// 		user: 'blah' //not actual output
+// 	}
+
+function respond(res, status, apiCall, data) {
+	status = status || 200;
+
+	res.send({
+		status: status,
+		call: apiCall,
+		data: data,
+	});
+}
+
+function errorRespond(res, status, apiCall, ex){
+	respond(res, status, apiCall)
+	if (ex) throw ex;
+}
+
+module.exports = {
+	respond: respond
+}
