23445c7a70a18f9dc763a61ef3061d97a63c7431
Collin Lane <cjlane@ucsc.edu>
2016-01-30 20:11:55 -0800

    modified server.js
    - added debug info

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose.connect('mongodb://' + config.db.host + '/' + config.db.name);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
-	console.log("Server Started.");
+	console.log("Server Started on Port ." + config.server.port);
 });
 
 // Initialize express app
