befd5dcf6c5304c0fa2278cef54879e3a1f23ea0
Collin Lane <cjlane@ucsc.edu>
2016-01-31 02:46:40 -0800

    modified routes.js

--- objects/routes.js
+++ objects/routes.js
@@ -85,7 +85,7 @@ function createUser(req, res) {
 		if (ex) throw ex;
 
 		console.log("1: " + user);
-		User.save(user, function(ex) {
+		user.save(user, function(ex) {
 			console.log("2: " + user);
 			if (ex) throw ex;
 
